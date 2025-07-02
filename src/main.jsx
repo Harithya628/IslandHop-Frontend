d3f552709b29ae3525d917e1d01171fe93f6cb8c
Threadstone15 <117582139+Threadstone15@users.noreply.github.com>
2025-07-02 22:31:12 +0530

    explore

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId={clientId}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </GoogleOAuthProvider>,
 )
